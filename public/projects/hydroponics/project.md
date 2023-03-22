493c6dfc66004fc358e13a60851f49b22452cdbb
Wolf <caffeinate@msn.com>
2023-03-22 15:56:36 -0400

    Mobile friendly project image sizes

--- public/projects/hydroponics/project.md
+++ public/projects/hydroponics/project.md
@@ -4,13 +4,19 @@ It was 2019, the summer before high school. Many projects were coming to a close
 
 # Moldy Strawberries
 
-![Strawberry grow tower](strawberry_tower.webp|width=400|float=right)
+![Strawberry grow tower](strawberry_tower.webp|width=35|float=right)
 
 ## Setup
 The first phase of the closet garden was, in fact, not actually hydroponics. My research began by looking for fast growing, easily maintainable plants, and settled on strawberries. I bought some seeds on ebay, and decided on a grow tower to maximize the area of my closet. If successful, there'd be room for many more towers, but I'd start with one. Meanwhile, I set up an Arduino system to automatically disperse water from a large bucket when the soil dried up, using a soil moisture sensor. It turned out to not water often enough, or over-water and leak, so I pivoted to timer based watering. For lighting, I settled on a sketchy, cheap Amazon grow light, which melted itself and set off my GFCI outlet. The replacement one was better, but still wasn't bright enough, and then the third batch worked well, though it heated up my closet to an ambient 85.  
 
-![Strawberry fruit](strawberries.webp)
 ## Success
+
+![Strawberry fruit](strawberries.webp|width=20|float=left)
+
 Within a few weeks, most of the strawberries germinated, and the plants began thriving. They began branching out, and runners (baby plants attached to the mother plant) started shooting out. Within weeks, flowers appeared, and then the first few fruits. They were juicy, bright red, and perfect—until the mold started. I tried adding a dehumidifier, and bought Neem oil as a natural fungicide to protect the edibility of the fruit, but it was futile. After caving to synthetic fungicide, I rid the plants of fungus, and effectively made the plant ornamental for months to come. After eventually giving away the plants so that they could thrive outside, it was back to the drawing board.
 
 # Hydroponics
+
+![Basil grow tower](basil_tower.webp|width=30|float=right)
+
+For my first attempt, I decided to go with an Amazon [basil grow tower](https://www.amazon.com/gp/product/B0787F9LG7/), since it claimed to provide an all-in-one system that just needed assembly. While it did indeed assemble, and after the first few weeks of figuring out nutrient ratios and water acidity, basil started growing, I quickly ran into another batch of problems: the small tubes were being jammed by the ever-growing basil plants' roots. The larger the plants got, the more unwieldy the system became, and the less water the plants received. Moreover, cleaning the system was virtually impossible without removing all the plants in the system and taking apart the tubes. A new method would be needed, but I established that hydroponic basil had potential. This time, I'd go my own way.
--- src/pages/projects/[projectId].js
+++ src/pages/projects/[projectId].js
@@ -1,28 +1,57 @@
 import MainLayout from '@/components/layouts/MainLayout';
 import Tile from '@/components/misc/Tile';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import { worker as projectFromId } from '../api/projects/by_id';
 import rehypeRaw from 'rehype-raw'
+import ReactDOMServer from 'react-dom/server';
 
 const Project = ({ projectId, projectData }) => {
     const [ projectMd, setProjectMd ] = useState('Loading...')
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     useEffect(() => {
         fetch(`/projects/${projectId}/project.md`)
             .then(res => res.text())
             .then(text => {
+                console.log(windowWidth)
                 const replacer = (match, alt, path, width, height, float) => {
-                    console.log(float)
-                    const replaced = `<img src="${projectId}/${path}" alt="${alt}" style="float: ${float ? float : 'right'};${width ? `width: ${width}px` : ""};${height ? `height: ${height}px;` : ""}"/>`
+                    const mobileExtraWidth = 40
+                    // Ideal width is the width of the image, plus 20 pixels if the window is less
+                    // than 400 pixels wide
+                    let idealWidth = Number(width) + ((windowWidth < 400) ? mobileExtraWidth : 0)
+                    // Map the idealWidth to be out of 100, so that it can be used as a percentage
+                    // of the window's width
+                    idealWidth = (100/(100+mobileExtraWidth)) * idealWidth
+
+                    const replaced = <img
+                        src={`${projectId}/${path}`}
+                        alt={alt}
+                        style={{
+                        float: float ? `${float}` : 'right',
+                        width: width ? `${idealWidth}%` : height ? "" : "25%",
+                        height: height ? `${height}px` : "",
+                        marginRight: float == "left" ? "1rem" : "",
+                        marginLeft: float == "right" ? "1rem" : "",
+                        }}
+                    />
                     console.log(replaced)
-                    return replaced
+                    return ReactDOMServer.renderToString(replaced)
                 }
 
                 text = text.replaceAll(/!\[(.*)\]\((.*\.webp)\|?(?:width=(\d+))?\|?(?:height=(\d+))?\|?(?:float=([a-z]+))?\)/g, replacer);
                 setProjectMd(text)
             })
-    }, [projectId])
+    }, [projectId, windowWidth])
 
     return (
         <MainLayout header={ projectData.name }>
