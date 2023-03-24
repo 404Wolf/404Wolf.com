4ebe7c9bfedaeb2e130f2c53a545e26dc8bf2e2d
Wolf <caffeinate@msn.com>
2023-03-24 01:50:28 -0400

    Fix tile titling layering

--- src/components/misc/Tile.js
+++ src/components/misc/Tile.js
@@ -3,7 +3,7 @@ import Typewriter from "typewriter-effect";
 const Tile = ( {title, className, children, direction="left" } ) => {
     return (
         <div className="h-full relative">
-            {title && <div className={ `text-center absolute -translate-y-[1.15rem] ${direction == "left" ? "-translate-x-[.9rem]": "translate-x-[.9rem] right-0" } bg-gray-700 text-white text-lg p-1 rounded-full w-24 sm:w-32` }>
+            {title && <div className={ `text-center absolute -translate-y-[1.15rem] ${direction == "left" ? "-translate-x-[.9rem]": "translate-x-[.9rem] right-0" } bg-gray-700 text-white text-lg p-[.15rem] rounded-full w-24 sm:w-32 z-50` }>
                 <h2 className="text-xl sm:text-2xl text-bold">
                     <Typewriter 
                         onInit={(typewriter) => {typewriter.typeString(title).start()}}
