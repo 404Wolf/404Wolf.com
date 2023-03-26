029086bc9c63ad74c64c82afb1fe343845c3391b
Wolf <caffeinate@msn.com>
2023-03-26 14:42:43 -0400

    Fix margin

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -22,12 +22,14 @@ interface ProjectsProps {
 const Projects = ({ projects }: ProjectsProps) => {
     return (
         <MainLayout header="Projects">
-            <Tile className="mb-8 markdown">
-                <p>
-                    This page is to showcase some of the projects I've worked on. It's a healthy blend of personal projects, academic projects, and more. Not all the projects are code-related, but many are. Each project has its own page with more information about it, so feel free to click on any of them to learn more. If you have any questions, feel free to contact me. More projects will be coming soon!
-                </p>
-                <Navbar/>
-            </Tile>
+            <div className="mb-8">
+                <Tile className="markdown">
+                    <p>
+                        This page is to showcase some of the projects I've worked on. It's a healthy blend of personal projects, academic projects, and more. Not all the projects are code-related, but many are. Each project has its own page with more information about it, so feel free to click on any of them to learn more. If you have any questions, feel free to contact me. More projects will be coming soon!
+                    </p>
+                    <Navbar/>
+                </Tile>
+            </div>
             <Tile title="Projects">
                 <ProjectsTile 
                     projects={ projects }
