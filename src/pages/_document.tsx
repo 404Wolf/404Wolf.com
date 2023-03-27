59927a95aa808a4c5b5a0f604294db5d8df91597
Wolf <caffeinate@msn.com>
2023-03-26 22:40:39 -0400

    Increase max size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="en" className="p-[4.5%] lg:px-[10%] xl:px-[15%] max-w-[91rem] mx-auto">
+        <Html lang="en" className="p-[4.5%] lg:px-[10%] xl:px-[15%] max-w-[100rem] mx-auto">
             <Head/>
             <body>
                 <Main />
