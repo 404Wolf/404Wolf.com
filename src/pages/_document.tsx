d13627fc42c9facc148cfe407efbe4106c7bf8da
Wolf <caffeinate@msn.com>
2023-03-26 22:36:28 -0400

    Add overall page max size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="en" className="p-[4.5%] lg:px-[10%] xl:px-[15%]">
+        <Html lang="en" className="p-[4.5%] lg:px-[10%] xl:px-[15%] max-w-[91rem] mx-auto">
             <Head/>
             <body>
                 <Main />
