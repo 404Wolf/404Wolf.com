5910a5e81fb8d0dd8c2730078f0f312d7b93a2eb
Wolf <caffeinate@msn.com>
2023-03-27 22:54:12 -0400

    Add tab title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en" className="p-[4.5%] lg:px-[10%] xl:px-[15%] max-w-[100rem] mx-auto">
-            <Head/>
+            <Head>
+                <title>Wolf Mermelstein</title>
+                <meta name="description" content="Wolf Mermelstein Personal Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
