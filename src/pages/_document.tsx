fa1026cff2ed77e902e4a230e6a81eeb56742186
Wolf <caffeinate@msn.com>
2023-03-27 23:35:49 -0400

    Add metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,13 @@ export default function Document() {
         <Html lang="en" className="p-[4.5%] lg:px-[10%] xl:px-[15%] max-w-[100rem] mx-auto">
             <Head>
                 <title>Wolf Mermelstein</title>
-                <meta name="description" content="Wolf Mermelstein Personal Website" />
+                <meta name="title" content="Wolf Mermelstein Personal Website"/>
+                <meta name="description" content="Enter the world of a creative student with a passion for tinkering, code, Latin tabletop, and more. Discover a portfolio of projects, blogs, and more."/>
+                <meta name="keywords" content="portfolio, personal, creative, curious, expansive, projects, blogs, Wolf Mermelstein, Mermelstein, New York, Brooklyn, engineering, design, tinkering, exploration, STEM, interdisciplinary, maker, explorer, contrive, create, empassion"/>
+                <meta name="robots" content="index, follow"/>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                <meta name="language" content="English"/>
+                <meta name="author" content="Wolf Mermelstein"/>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
