1c5c7a7474d161d7612590831672b34b12fbb5a8
Wolf <caffeinate@msn.com>
2023-04-26 20:38:19 -0400

    Update Eclectic English Vocab project

--- src/layouts/PostLayout.tsx
+++ src/layouts/PostLayout.tsx
@@ -1,13 +1,13 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import rehypeRaw from "rehype-raw";
 import Tile from "../components/misc/Tile";
-import MdImage from "../components/misc/MdImage";
 import MainLayout from "./MainLayout";
 import { useEffect, useState } from "react";
 import useSize from "@/utils/useSize";
 import PostData from "@/components/posts/PostData";
 import { parseMd } from "@/utils/parseMd";
-import { randomListItem, toTitleCase } from "@/utils/misc";
+import { toTitleCase } from "@/utils/misc";
 import Image from "next/image";
 
 interface PostLayoutProps {
@@ -34,7 +34,10 @@ const PostLayout = ({ postId, type, title }: PostLayoutProps) => {
     }, [postData, postId, windowSize]);
 
     return (
-        <MainLayout title={postData ? postData.name : toTitleCase(type)} header={false}>
+        <MainLayout
+            title={postData ? postData.name : toTitleCase(type)}
+            header={false}
+        >
             <div className={postData ? "mt-[5px]" : ""}>
                 {postData && (
                     <Tile title="Overview" className="overflow-auto" direction="right">
@@ -56,9 +59,30 @@ const PostLayout = ({ postId, type, title }: PostLayoutProps) => {
                 <div className="m-6" />
                 <Tile className="overflow-auto" title={title} direction="right">
                     <div>
-                    <ReactMarkdown className="markdown" rehypePlugins={[rehypeRaw]}>
-                        {postMd}
-                    </ReactMarkdown>
+                        <ReactMarkdown
+                            className="markdown"
+                            rehypePlugins={[rehypeRaw]}
+                            components={{
+                                code({ node, inline, className, children, ...props }) {
+                                    const match = /language-(\w+)/.exec(className || "");
+                                    return !inline && match ? (
+                                        <SyntaxHighlighter
+                                            {...props}
+                                            children={String(children).replace(/\n$/, "")}
+                                            language={match[1]}
+                                            PreTag="div"
+                                            customStyle={{ borderRadius: "12px"}}
+                                        />
+                                    ) : (
+                                        <code {...props} className={className}>
+                                            {children}
+                                        </code>
+                                    );
+                                },
+                            }}
+                        >
+                            {postMd}
+                        </ReactMarkdown>
                     </div>
                 </Tile>
             </div>
