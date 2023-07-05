a816d1747463f0394649b65e9a641ee23e1ef23c
Wolf <caffeinate@msn.com>
2023-07-05 12:27:39 -0400

    Begin work on standardized post page

--- src/pages/posts/[type]/index.tsx
+++ src/pages/posts/[type]/index.tsx
@@ -1,19 +1,68 @@
-import PostsIndexLayout from "@/layouts/PostsIndexLayout";
 import Head from "next/head";
+import typeDescriptions from "@/metadata/types.json";
+import MainLayout from "@/layouts/MainLayout";
+import Tile from "@/components/misc/Tile";
+import { useEffect, useState } from "react";
+import ExtendedPostCard from "@/components/posts/ExtendedPostCard";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { GetServerSideProps, GetStaticPaths, GetStaticPathsResult } from "next";
 
-const Blogs = () => {
+const prisma = new PrismaClient();
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const type: string | null = params
+        ? typeof params.type === "string"
+            ? params.type
+            : null
+        : null;
+
+    const posts = await prisma.post.findMany({
+        where: { type: type?.slice(0, -1) || "" },
+    });
+    const covers: string[] = await Promise.all(
+        posts
+            .map((post) => post.covers)
+            .flatMap((coverId) =>
+                typeof coverId === "string"
+                    ? prisma.resource.findUnique({ where: { id: coverId } })
+                    : null
+            )
+            .filter((cover) => typeof cover === "object")
+    );
+
+    return {
+        props: {
+            type: type,
+            posts: posts,
+            covers: covers,
+        },
+    };
+};
+
+interface PostsProps {
+    type: string;
+    posts: Prisma.PostSelect[];
+    covers: Prisma.ResourceSelect[]
+}
+
+const PostsIndexLayout = ({ type, posts, covers }: PostsProps) => {
     return (
-        <>
-            <Head>
-                <title>Blogs</title>
-            </Head>
-            <PostsIndexLayout header="Blogs" type="blogs">
-                Here I'll be posting various blogs about various topics. This is still a
-                very new page, so there's not much here yet. I'll be adding more content
-                soon!
-            </PostsIndexLayout>
-        </>
+        <MainLayout
+            title={type}
+            headerChildren={<div className="markdown">typeDescriptions[type]</div>}
+        >
+            <div className="-mt-3">
+                <Tile>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-7 md:gap-y-10 p-1 pt-2">
+                        {posts &&
+                            posts.map((post) => (
+                                <ExtendedPostCard tags={post.tags} post={post} />
+                            ))}
+                    </div>
+                </Tile>
+            </div>
+        </MainLayout>
     );
 };
 
-export default Blogs;
+export default PostsIndexLayout;
