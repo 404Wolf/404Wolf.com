63db5dc7a23007d0515c827cd0c79d8e92de0d6e
Wolf <caffeinate@msn.com>
2023-07-05 21:33:16 -0400

    Change arg to post's tags

--- src/components/posts/fetchPostIcon.tsx
+++ src/components/posts/fetchPostIcon.tsx
@@ -1,16 +1,15 @@
-import PostData from "./PostData";
 import { IoMdSchool } from "react-icons/io";
 import { IoPersonSharp } from "react-icons/io5";
 import { BsQuestionLg } from "react-icons/bs";
 
-const fetchPostIcon = (post: PostData) => {
+const fetchPostIcon = (tags: string[]) => {
     const postIconClass = "rounded-full bg-slate-600 p-2 w-9 h-9";
     const postIconStyle = { filter: "invert(100%)", width: "100%", height: "100%" };
 
     let postIcon;
-    if (post.tags.includes("academic")) {
+    if (tags.includes("academic")) {
         postIcon = <IoMdSchool style={postIconStyle} />;
-    } else if (post.tags.includes("personal")) {
+    } else if (tags.includes("personal")) {
         postIcon = <IoPersonSharp style={postIconStyle} />;
     } else {
         postIcon = <BsQuestionLg style={postIconStyle} />;
