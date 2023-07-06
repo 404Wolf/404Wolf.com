173d7a00655f7d9c57dcd2c781d12cea27a6bfba
Wolf <caffeinate@msn.com>
2023-07-05 21:32:53 -0400

    Modify props of ExtendedPostCard

--- src/components/posts/ExtendedPostCard.tsx
+++ src/components/posts/ExtendedPostCard.tsx
@@ -1,5 +1,4 @@
-import { randomListItem, toTitleCase } from "@/utils/misc";
-import PostData from "./PostData";
+import { toTitleCase } from "@/utils/misc";
 import Image from "next/image";
 import Tag from "../misc/Tag";
 import Link from "next/link";
@@ -8,29 +7,40 @@ import fetchPostIcon from "./fetchPostIcon";
 const nullDescription = "Description coming soon!";
 
 interface ExtendedPostCardProps {
-    post: PostData | null;
+    coverUrl: string;
+    coverAlt?: string;
+    path: string;
+    title: string;
+    description: string;
+    date: string;
     tags?: string[];
 }
 
-const ExtendedPostCard = ({ post, tags }: ExtendedPostCardProps) => {
-    if (!post) return <div className="h-40 bg-slate-400 rounded-xl"></div>;
-
-    const postDescription = post.description ? post.description : nullDescription;
-
+const ExtendedPostCard = ({
+    coverUrl,
+    coverAlt,
+    path,
+    title,
+    description,
+    date,
+    tags,
+}: ExtendedPostCardProps) => {
     return (
-        <Link href={post.path}>
+        <Link href={path}>
             <div className="h-[9.8rem] w-full relative container drop-shadow-md hover:drop-shadow-lg hover:scale-[102%] duration-200">
                 <Image
                     fill
-                    alt={`${post.name} cover image`}
-                    src={post.covers[0]}
+                    alt={coverAlt || `${title}'s cover image`}
+                    src={coverUrl}
                     className="object-cover rounded-xl z-0"
                 />
 
-                <div className="absolute z-50 -top-2 -right-2">{fetchPostIcon(post)}</div>
+                <div className="absolute z-50 -top-2 -right-2">
+                    {fetchPostIcon(tags || [])}
+                </div>
 
                 <h1 className="z-50 px-2 py-px text-white text-[15px] absolute -top-2 -left-2 bg-slate-600 rounded-full">
-                    {post.name}
+                    {title}
                 </h1>
 
                 <div className="flex gap-x-[4px] absolute -bottom-1 -left-2">
@@ -45,11 +55,11 @@ const ExtendedPostCard = ({ post, tags }: ExtendedPostCardProps) => {
                 </div>
 
                 <h1 className="z-50 px-2 py-px text-white text-lg absolute -bottom-2 -right-2 bg-slate-600 rounded-full">
-                    {post.date}
+                    {date}
                 </h1>
 
                 <p className="border-t-2 border-l-2 border-slate-500 absolute right-0 bottom-0 z-30 w-5/6 md:w-2/3 indent-4 bg-gray-200 rounded-tl-3xl overflow-hidden h-[55%] md:h-[62%] p-[4px] text-[11px]">
-                    {postDescription}
+                    {description}
                 </p>
             </div>
         </Link>
