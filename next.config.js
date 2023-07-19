fa69f4dd28ea70b637985a9085b683a1a30efbab
Wolf <caffeinate@msn.com>
2023-07-19 13:32:08 -0400

    Add /posts redirect

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['wolf-mermelstein-personal-website.s3.us-east-2.amazonaws.com'],
-  },
-  experimental: {
-    serverActions: true,
-  },
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ['wolf-mermelstein-personal-website.s3.us-east-2.amazonaws.com'],
+    },
+    experimental: {
+        serverActions: true,
+    },
+    async redirects() {
+        return [
+            {
+                source: "/posts",
+                destination: "/",
+                permanent: false,
+            }
+        ]
+    }
 }
-
-module.exports = nextConfig
