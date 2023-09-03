3a6f12a9ab636d8e8b373b31dde285f3754679ec
Wolf <caffeinate@msn.com>
2023-09-03 13:28:30 -0400

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,27 @@ import 'react-tooltip/dist/react-tooltip.css'
 
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <SessionProvider session={session}>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+
+            <Script strategy="lazyOnload">
+                {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                            page_path: window.location.pathname,
+                            });
+                        `}
+            </Script>
+
             <Component {...pageProps} />
         </SessionProvider>
     );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
-        <Html lang="en" className="py-[6%] px-[4.5%] md:px-[6%] lg:px-[11%] xl:px-[15%] max-w-[100rem] mx-auto">
+        <Html
+            lang="en"
+            className="py-[6%] px-[4.5%] md:px-[6%] lg:px-[11%] xl:px-[15%] max-w-[100rem] mx-auto"
+        >
             <Head>
-                <meta name="title" content="Wolf Mermelstein Personal Website"/>
-                <meta name="description" content="Enter the world of a creative student who loves tinkering, coding, Latin, tabletop, and more. Discover a portfolio of projects and blogs, and get their contacts."/>
-                <meta name="keywords" content="portfolio, personal, creative, curious, expansive, projects, blogs, Wolf Mermelstein, Mermelstein, New York, Brooklyn, engineering, design, tinkering, exploration, STEM, interdisciplinary, maker, explorer, contrive, create, empassion"/>
-                <meta name="robots" content="index, follow"/>
-                <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-                <meta name="language" content="English"/>
-                <meta name="author" content="Wolf Mermelstein"/>
+                <meta name="title" content="Wolf Mermelstein Personal Website" />
+                <meta
+                    name="description"
+                    content="Enter the world of a creative student who loves tinkering, coding, Latin, tabletop, and more. Discover a portfolio of projects and blogs, and get their contacts."
+                />
+                <meta
+                    name="keywords"
+                    content="portfolio, personal, creative, curious, expansive, projects, blogs, Wolf Mermelstein, Mermelstein, New York, Brooklyn, engineering, design, tinkering, exploration, STEM, interdisciplinary, maker, explorer, contrive, create, empassion"
+                />
+                <meta name="robots" content="index, follow" />
+                <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="language" content="English" />
+                <meta name="author" content="Wolf Mermelstein" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
+            
+
             <body>
                 <Main />
                 <NextScript />
             </body>
         </Html>
-    )
+    );
 }
