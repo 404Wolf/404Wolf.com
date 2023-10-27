d8f38df4eeedfd0af65d3aa0c76e5089186f29cf
Wolf <caffeinate@msn.com>
2023-10-27 18:16:59 -0400

    Redownload resume on each page load

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,10 +1,37 @@
-export async function getServerSideProps() {
-    return {
-            redirect: {
-            destination: '/resume.pdf',
-            permanent: true,
-        },
-    };
+import { GetServerSideProps } from 'next';
+import React, { useEffect } from 'react';
+import fs from 'fs';
+import path from 'path';
+import { useRouter } from 'next/router';
+import StatusLayout from '@/layouts/StatusLayout';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const PDF_URL = 'https://github.com/404Wolf/Resume/raw/master/resume.pdf';
+  const pdfResponse = await fetch(PDF_URL);
+  const pdfData = await pdfResponse.arrayBuffer();
+  const pdfPath = path.join(process.cwd(), 'public', 'resume.pdf');
+  fs.writeFileSync(pdfPath, Buffer.from(pdfData));
+
+  return {
+    props: {
+      pdfPath: '/resume.pdf',
+    },
+  };
+};
+
+interface PDFPageProps {
+  pdfPath: string;
 }
 
-export default function Page() { <div>Redirecting...</div> }
+const PDFPage = ({ pdfPath } : {pdfPath: string}) => {
+    const router = useRouter();
+
+    useEffect(() => {
+      // Redirect the user to the PDF file URL
+      router.push(pdfPath);
+    }, [router, pdfPath]);
+
+    return <StatusLayout children={"Loading resume..."} name={"Loading..."} />;
+};
+
+export default PDFPage;
