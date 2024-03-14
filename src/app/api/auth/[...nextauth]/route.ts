1c5e66ed35b6594193d1869aa261cec108597b20
Wolf <wolfmermelstein@gmail.com>
2024-03-13 22:21:49 -0400

    Fix auth system

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-const prisma = new PrismaClient()
-
-/**
- * The authentication options for next auth.
- */
-export const authOptions: AuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-    callbacks: {
-        async signIn({ user }: { user: { id: string } }) {
-            if ((await prisma.user.findUnique({
-                where: {
-                    id: user.id
-                }
-            })) === null)
-                return "https://www.youtube.com/watch?v=p7YXXieghto"
-            else return true
-        }
-    }
-};
-
+import authOptions from "@/app/auth";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
