ac173704b48db8b49f8ac646e235634d299fd66c
Wolf <wolfmermelstein@gmail.com>
2024-03-16 04:05:23 -0400

    Fix prisma import

--- transfers/download.ts
+++ transfers/download.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from ".prisma/client";
+import { PrismaClient } from "@prisma/client";
 import {mkdirSync, readdirSync, writeFileSync} from "fs";
 import {downloadFile} from "./utils";
 
