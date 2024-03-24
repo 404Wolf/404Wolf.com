6fbf317d6cfa52593d55517fcdaea2a04d53160d
Wolf <wolfmermelstein@gmail.com>
2024-03-24 02:02:59 -0400

    Remove literal )

--- src/components/misc/Tiles/Title.tsx
+++ src/components/misc/Tiles/Title.tsx
@@ -51,7 +51,7 @@ const TileTitle = ({
                         />
                         {!ready && <>&nbsp;</>}
                     </span>
-                    )</>) : (
+                    </>) : (
                     title
                 )}
             </h2>
