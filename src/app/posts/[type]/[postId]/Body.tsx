1e0271b4eaa1e80bd2ddb53651475f2c95411b8e
Wolf <wolfmermelstein@gmail.com>
2024-04-23 21:26:55 -0400

    Remove textClasses

--- src/app/posts/[type]/[postId]/Body.tsx
+++ src/app/posts/[type]/[postId]/Body.tsx
@@ -34,7 +34,7 @@ export default function Body({
     const windowWidth = useWindowWidth({ wait: 100, leading: true });
 
     const markdownArea = useMemo(() => {
-        return <Markdown markdown={markdown} resourceMap={resources} textClasses={"xl:px-[20%]"} />
+        return <Markdown markdown={markdown} resourceMap={resources} />
     }, [])
 
     return (
