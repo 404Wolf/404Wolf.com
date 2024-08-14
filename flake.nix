3e5f30990e01f79b76390ca10fe1bcd8aea33940
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-08-14 17:40:34 -0400

    Add bun for development

--- flake.nix
+++ flake.nix
@@ -6,20 +6,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-    ( system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      {
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.nodejs_22
             pkgs.corepack_22
+            pkgs.bun
             pkgs.openssl
             pkgs.prisma-engines
           ];
           shellHook = ''
-            pnpm i
             export PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING=1
             export PRISMA_MIGRATION_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
             export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
