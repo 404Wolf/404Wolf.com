b7142639746af309a168cd5b459a55c50b5adb1c
Wolf Mermelstein <wolf@404wolf.com>
2025-09-01 22:42:40 -0400

    Make rss not public

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,8 +1,9 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { createRSSFeed } from "./worker";
+// import { createRSSFeed } from "./worker";
 
 export async function GET(_req: NextRequest) {
-  return new NextResponse(await createRSSFeed(), {
-    headers: { "Content-Type": "application/rss+xml" },
-  });
+  return NextResponse.json({ error: "Not found" }, { status: 404 }); // for now
+  // return new NextResponse(await createRSSFeed(), {
+  //   headers: { "Content-Type": "application/rss+xml" },
+  // });
 }
